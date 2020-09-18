ba050fbc3d4948daf882235df2ebd6933eace897
ctrivinoe <ctrivinoe@gmail.com>
2020-09-18 13:16:31 +0200

    tables mapped

---
+++ src/main/java/com/platzimarket/persistence/entity/Categoria.java
@@ -0,0 +1,41 @@
+package com.platzimarket.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    private String descripcion;
+
+    private Boolean estadao;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstadao() {
+        return estadao;
+    }
+
+    public void setEstadao(Boolean estadao) {
+        this.estadao = estadao;
+    }
+}
